7cecc184d9f9d4fcf5a0e71f3ef5214f770a6168
Grea1722 <alexs17@live.com.mx>
2025-06-09 15:47:05 -0700

    Agregadas secciones Pricing y benefits

--- resources/js/views/landing/LandingPage.vue
+++ resources/js/views/landing/LandingPage.vue
@@ -2,7 +2,7 @@
     <div>
         <Hero />
         <Features />
-        <!-- <DashboardPreview /> -->
+        <DashboardPreview />
         <Benefits />
         <Pricing />
         <CTA />
@@ -12,12 +12,11 @@
 <script>
 import Benefits from "./Benefits.vue";
 import CTA from "./CTA.vue";
-// import DashboardPreview from "./DashboardPreview.vue";
+import DashboardPreview from "./DashboardPreview.vue";
 import Features from "./Features.vue";
 import Footer from "./Footer.vue";
 import Hero from "./Hero.vue";
 import Pricing from "./Pricing.vue";
-import CTA from "./CTA.vue";
 
 export default {
     components: {
